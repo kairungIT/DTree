8dffc085e8a8a701d7184cc2265bedd7c30ecfbe
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-05 15:15:34 +0700

    Update DTree.py

--- DTree.py
+++ DTree.py
@@ -7,3 +7,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 st.header("Decision Tree for classification")
+df = pd.read_csv("/data/iris.csv")
+st.write(df.head(10))
+
+features=['sepal.length', 'sepal.width', 'petal.length', 'petal.width']
+X = df.drop('variety',axis=1)
+y = df['variety']
+
+x_train,x_test,y_train,y_test =train_test_split(X,y,test_size=0.3,random_state=200)
+ModelDtree = DecisionTreeClassifier()
+dtree =ModelDtree.fit(x_train,y_train)
+tree.plot_tree(dtree, feature_names=features)
