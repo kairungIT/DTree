00b023c86a341eab9038661a551efb8a94c8ee49
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-05 15:11:31 +0700

    .

---
+++ DTree.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+st.header("Decision Tree for classification")
