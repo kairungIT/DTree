6cd28cb85a7fdd66e14bdc7160a468088b93fe33
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-08 14:33:48 +0700

    .

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.page_link("Home.py", label="Home", icon="🏠")
-st.page_link("pages/DTree.py", label="DecisionTree", icon="1️⃣")
-st.page_link("pages/NaiveBaye.py", label="NaiveBaye", icon="2️⃣", disabled=True)
+st.page_link("pages/DTree.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/NaiveBaye.py", label="Page 2", icon="2️⃣", disabled=True)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
